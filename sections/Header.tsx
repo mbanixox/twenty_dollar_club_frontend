7fd573a7f8620082d156ab58ffe75642394025c5
Nixox <46408125+mbanixox@users.noreply.github.com>
2025-12-29 21:43:31 +0300

    Add conditional rendering for sign-out and sign-in based on user session

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -1,17 +1,34 @@
 import SignInDialog from "@/components/SignInDialog";
 import { Button } from "@/components/ui/button";
+import { signOut } from "@/lib/auth/actions";
+import { getSession } from "@/lib/auth/session";
 import Link from "next/link";
 
-const Header = () => {
+const Header = async () => {
+  const session = await getSession();
+
   return (
     <header className="px-5 py-3 font-outfit sticky top-0 z-10 transition-all duration-300 ease-in-out bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 shadow-2xl">
       <div className="flex justify-between items-center">
         <div>TwentyDollarClub</div>
         <div className="flex items-center gap-5 text-black">
-          <SignInDialog />
-          <Link href="/register">
-            <Button size="sm">Become a member</Button>
-          </Link>
+          {session ? (
+            <form
+              action={async () => {
+                "use server";
+                await signOut();
+              }}
+            >
+              <Button size="sm">Sign Out</Button>
+            </form>
+          ) : (
+            <>
+              <SignInDialog />
+              <Link href="/register">
+                <Button size="sm">Become a member</Button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </header>
--- sections/HomeHero.tsx
+++ sections/HomeHero.tsx
@@ -1,8 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { getSession } from "@/lib/auth/session";
 import Image from "next/image";
 import Link from "next/link";
 
-const HomeHero = () => {
+const HomeHero = async () => {
+  const session = await getSession();
+
   return (
     <section className="w-full min-h-132.5 flex justify-center items-center flex-col py-10 px-6 drop-shadow bg-cover bg-center">
       <Image
@@ -18,9 +21,11 @@ const HomeHero = () => {
       <p className="text-xl tracking-tight mt-6 mb-10">
         Join a trusted community where collective empowerment leads to wealth.
       </p>
-      <Link href="/register">
-        <Button size="sm">Become a member</Button>
-      </Link>
+      {!session && (
+        <Link href="/register">
+          <Button size="sm">Become a member</Button>
+        </Link>
+      )}
     </section>
   );
 };
