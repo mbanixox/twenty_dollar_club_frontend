c18455d0fe1ce2a00487f3e3145c06f74ecc9640
Nixox <46408125+mbanixox@users.noreply.github.com>
2025-12-30 13:32:42 +0300

    Add table templates
    -Will reuse and populate

--- app/dashboard/(dashboard)/members/page.tsx
+++ app/dashboard/(dashboard)/members/page.tsx
@@ -1,5 +1,91 @@
-const Page = () => {
-  return <div>Members Page</div>;
-};
+import { columns, Payment } from "../../../../components/tables/columns"
+import { DataTable } from "../../../../components/tables/data-table"
 
-export default Page;
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "a@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "b@example.com",
+    },{
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "c@example.com",
+    },{
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "d@example.com",
+    },{
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "e@example.com",
+    },{
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "f@example.com",
+    },{
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "g@example.com",
+    },{
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "h@example.com",
+    },{
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "i@example.com",
+    },{
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "j@example.com",
+    },{
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "k@example.com",
+    },{
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "l@example.com",
+    },{
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },{
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "n@example.com",
+    },
+    // ...
+  ]
+}
+
+export default async function DemoPage() {
+  const data = await getData()
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  )
+}
---
+++ components/tables/data-table.tsx
@@ -0,0 +1,135 @@
+"use client";
+
+import * as React from "react";
+import {
+  ColumnDef,
+  SortingState,
+  VisibilityState,
+  ColumnFiltersState,
+  flexRender,
+  useReactTable,
+  getCoreRowModel,
+  getSortedRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+} from "@tanstack/react-table";
+
+import { Input } from "@/components/ui/input";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { DataTablePagination } from "@/components/tables/DataTablePagination";
+import { DataTableViewOptions } from "@/components/tables/DataTableViewOptions";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+
+  const [rowSelection, setRowSelection] = React.useState({});
+
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onRowSelectionChange: setRowSelection,
+    onColumnFiltersChange: setColumnFilters,
+    onColumnVisibilityChange: setColumnVisibility,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(), // Enable pagination to 10 rows per page
+    state: {
+      sorting,
+      rowSelection,
+      columnFilters,
+      columnVisibility,
+    },
+  });
+
+  return (
+    <div>
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filter emails..."
+          value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("email")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+        <DataTableViewOptions table={table} />
+      </div>
+
+      <div className="overflow-hidden rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <DataTablePagination table={table} />
+    </div>
+  );
+}
