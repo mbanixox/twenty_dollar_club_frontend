7255340a4c7aa9df7441d9a301a7cd0c4f9becb5
Nixox <46408125+mbanixox@users.noreply.github.com>
2026-01-01 14:21:26 +0300

    Wrap content in `SidebarInset` to handle content positions

--- app/dashboard/(dashboard)/layout.tsx
+++ app/dashboard/(dashboard)/layout.tsx
@@ -1,15 +1,18 @@
 import { AppSidebar } from "@/components/AppSidebar";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    
     <SidebarProvider>
       <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        {children}
-      </main>
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger />
+        </header>
+        <div className="flex flex-1 flex-col">
+          {children}
+        </div>
+      </SidebarInset>
     </SidebarProvider>
   );
 };
